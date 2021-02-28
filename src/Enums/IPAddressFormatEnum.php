20d4729f1647c804a1e618c78aaa68e0f1ff29c9
angelcamposm <angel.campos.m@outlook.com>
2021-02-28 21:41:14 +0100

    Added Class IPAddressFormatEnum.php like Enum

---
+++ src/Enums/IPAddressFormatEnum.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Acamposm\IPv4AddressConverter\Enums;
+
+class IPAddressFormatEnum
+{
+    public const BINARY = 0;
+    public const DECIMAL = 1;
+    public const HEXADECIMAL = 2;
+    public const LONG = 3;
+}
