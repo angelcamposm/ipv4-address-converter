a3becab5589e7ed11d956ed7a332c898bcb51834
angelcamposm <angel.campos.m@outlook.com>
2021-03-01 00:01:15 +0100

    Renamed copied Facade

--- src/Facades/IPv4AddressConverterFacade.php
+++ src/Facades/IPv4AddressConverterFacade.php
@@ -4,7 +4,7 @@ namespace Acamposm\IPv4AddressConverter\Facades;
 
 use Illuminate\Support\Facades\Facade;
 
-class SubnetCalculatorFacade extends Facade
+class IPv4AddressConverterFacade extends Facade
 {
     /**
      * Get the registered name of the component.
