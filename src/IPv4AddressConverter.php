cae5920cc13e37e97ecf66151645e5436a119ff9
angelcamposm <angel.campos.m@outlook.com>
2021-02-28 21:43:47 +0100

    Added Class IPv4AddressConverter

---
+++ src/IPv4AddressConverter.php
@@ -0,0 +1,167 @@
+<?php
+
+
+namespace Acamposm\IPv4AddressConverter;
+
+use Acamposm\IPv4AddressConverter\{
+    Converters\BinaryIPv4AddressConverter,
+    Converters\DecimalIPv4AddressConverter,
+    Converters\HexadecimalIPv4AddressConverter,
+    Converters\LongIPv4AddressConverter,
+    Enums\IPAddressFormatEnum as IPAddressFormat,
+    Interfaces\IPv4AddressConverterInterface,
+};
+
+class IPv4AddressConverter
+{
+    private int|string $address;
+    private IPv4AddressConverterInterface $converter;
+    private int $inputFormat;
+    private bool $withDotNotation = false;
+
+    /**
+     * IPv4AddressConverter constructor.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    public static function convert()
+    {
+        return new static();
+    }
+
+    /**
+     * @param string $address
+     * @return $this
+     */
+    public function fromBinary(string $address): IPv4AddressConverter
+    {
+        $this->address = $address;
+        $this->inputFormat = IPAddressFormat::BINARY;
+
+        return $this;
+    }
+
+    /**
+     * @param string $address
+     * @return $this
+     */
+    public function fromDecimal(string $address): IPv4AddressConverter
+    {
+        $this->address = $address;
+        $this->inputFormat = IPAddressFormat::DECIMAL;
+
+        return $this;
+    }
+
+    /**
+     * @param string $address
+     * @return $this
+     */
+    public function fromHexadecimal(string $address): IPv4AddressConverter
+    {
+        $this->address = $address;
+        $this->inputFormat = IPAddressFormat::HEXADECIMAL;
+
+        return $this;
+    }
+
+    /**
+     * @param int $address
+     * @return $this
+     */
+    public function fromLong(int $address): IPv4AddressConverter
+    {
+        $this->address = $address;
+        $this->inputFormat = IPAddressFormat::LONG;
+
+        return $this;
+    }
+
+    /**
+     * Set the output format as Binary string.
+     *
+     * @return IPv4AddressConverter
+     */
+    public function toBinary(): IPv4AddressConverter
+    {
+        $this->converter = new BinaryIPv4AddressConverter();
+
+        return $this;
+    }
+
+    /**
+     * Set the output format as Decimal string.
+     *
+     * @return IPv4AddressConverter
+     */
+    public function toDecimal(): IPv4AddressConverter
+    {
+        $this->converter = new DecimalIPv4AddressConverter();
+
+        return $this;
+    }
+
+    /**
+     * Set the output format as Hexadecimal string.
+     *
+     * @return IPv4AddressConverter
+     */
+    public function toHexadecimal(): IPv4AddressConverter
+    {
+        $this->converter = new HexadecimalIPv4AddressConverter();
+
+        return $this;
+    }
+
+    /**
+     * Set the output format as Long Integer.
+     *
+     * @return IPv4AddressConverter
+     */
+    public function toLong(): IPv4AddressConverter
+    {
+        $this->converter = new LongIPv4AddressConverter();
+
+        return $this;
+    }
+
+    /**
+     * Apply Dot notation to the output format.
+     *
+     * @return IPv4AddressConverter
+     */
+    public function withDotNotation(): IPv4AddressConverter
+    {
+        $this->withDotNotation = true;
+
+        return $this;
+    }
+
+    /**
+     *
+     * @return int|string
+     */
+    public function get(): int|string
+    {
+        $converter = $this->converter;
+
+        return match($this->inputFormat) {
+            IPAddressFormat::BINARY => $this->withDotNotation
+                ? $converter->fromBinary($this->address)->withDotNotation()->convert()
+                : $converter->fromBinary($this->address)->convert(),
+            IPAddressFormat::DECIMAL => $this->withDotNotation
+                ? $converter->fromDecimal($this->address)->withDotNotation()->convert()
+                : $converter->fromDecimal($this->address)->convert(),
+            IPAddressFormat::HEXADECIMAL => $this->withDotNotation
+                ? $converter->fromHexadecimal($this->address)->withDotNotation()->convert()
+                : $converter->fromHexadecimal($this->address)->convert(),
+            IPAddressFormat::LONG => $this->withDotNotation
+                ? $converter->fromLong($this->address)->withDotNotation()->convert()
+                : $converter->fromLong($this->address)->convert(),
+            default => $this->address,
+        };
+    }
+}
---
+++ src/Traits/MutableIPv4AddressTrait.php
@@ -0,0 +1,212 @@
+<?php
+
+namespace Acamposm\IPv4AddressConverter\Traits;
+
+trait MutableIPv4AddressTrait
+{
+    /**
+     * Apply dot notation to IP Address.
+     *
+     * @param string $address
+     * @param int $length
+     * @return string
+     */
+    private function applyDotNotationToAddress(string $address, int $length): string
+    {
+        return implode('.', str_split($address, $length));
+    }
+
+    /**
+     * Converts a binary IP Address to dotted decimal IP Address.
+     *
+     * @param string $address
+     * @return string
+     */
+    private function fromBinaryToDecimal(string $address): string
+    {
+        $octets = [];
+
+        foreach (str_split($address, 8) as $octet) {
+            $octets[] = bindec($octet);
+        }
+
+        return implode('.', $octets);
+    }
+
+    /**
+     * Converts a binary IP Address to a hexadecimal IP Address.
+     *
+     * @param string $address
+     * @return string
+     */
+    private function fromBinaryToHex(string $address): string
+    {
+        $octets = '';
+
+        foreach(str_split($address, 8) as $octet) {
+            $octets .= str_pad(dechex(bindec($octet)), 2, '0', STR_PAD_LEFT);
+        }
+
+        return strtoupper($octets);
+    }
+
+    /**
+     * Converts a binary IP Address to dotted hexadecimal IP Address.
+     *
+     * @param string $address
+     * @return string
+     */
+    private function fromBinaryToDottedHex(string $address): string
+    {
+        return $this->applyDotNotationToAddress($this->fromBinaryToHex($address), 2);
+    }
+
+    /**
+     * Converts a binary IP Address to long integer IP Address.
+     *
+     * @param string $address
+     * @return int
+     */
+    private function fromBinaryToLong(string $address): int
+    {
+        return $this->fromDecimalToLong($this->fromBinaryToDecimal($address));
+    }
+
+    /**
+     * Converts a dotted decimal IP Address to binary IP Address.
+     *
+     * @param string $address
+     * @return string
+     */
+    private function fromDecimalToBinary(string $address): string
+    {
+        $octets = '';
+
+        foreach (explode('.', $address) as $octet) {
+            $octets .= str_pad(decbin($octet), 8, '0', STR_PAD_LEFT);
+        }
+
+        return $octets;
+    }
+
+    /**
+     * Converts a dotted decimal IP Address to doted binary IP Address.
+     *
+     * @param string $address
+     * @return string
+     */
+    private function fromDecimalToDottedBinary(string $address): string
+    {
+        return $this->applyDotNotationToAddress($this->fromDecimalToBinary($address), 8);
+    }
+
+    /**
+     * Converts a dotted decimal IP Address to dotted hex IP Address.
+     *
+     * @param string $address
+     * @return string
+     */
+    private function fromDecimalToDottedHex(string $address): string
+    {
+        return $this->applyDotNotationToAddress($this->fromDecimalToHex($address), 2);
+    }
+
+    /**
+     * Converts a dotted decimal IP Address to a hex IP Address.
+     *
+     * @param string $address
+     * @return string
+     */
+    private function fromDecimalToHex(string $address): string
+    {
+        $octets = '';
+
+        foreach(explode('.', $address) as $octet) {
+            $octets .= str_pad(dechex($octet), 2, '0', STR_PAD_LEFT);
+        }
+
+        return strtoupper($octets);
+    }
+
+    /**
+     * Converts a doted decimal IP Address to long integer IP Address.
+     *
+     * @param string $address
+     * @return int
+     */
+    private function fromDecimalToLong(string $address): int
+    {
+        return ip2long($address);
+    }
+
+    /**
+     * Converts a dotted binary IP Address to doted decimal IP Address.
+     *
+     * @param string $address
+     * @return string
+     */
+    private function fromDottedBinaryToDecimal(string $address): string
+    {
+        $octets = [];
+
+        foreach (explode('.', $address) as $octet) {
+            $octets[] = bindec($octet);
+        }
+
+        return implode('.', $octets);
+    }
+
+    /**
+     * Converts a long integer IP Address to decimal IP Address.
+     *
+     * @param string $address
+     * @return string
+     */
+    private function fromLongToDecimal(string $address): string
+    {
+        return long2ip($address);
+    }
+
+    /**
+     * Converts a long integer IP Address to a dotted hex IP Address.
+     *
+     * @param string $address
+     * @return string
+     */
+    private function fromLongToDottedHex(string $address): string
+    {
+        return $this->fromDecimalToDottedHex(long2ip($address));
+    }
+
+    /**
+     * Converts a long integer IP Address to a hex IP Address.
+     *
+     * @param int $address
+     * @return string
+     */
+    private function fromLongToHex(int $address): string
+    {
+        return $this->fromDecimalToHex(long2ip($address));
+    }
+
+    /**
+     * Converts a long integer IP Address to binary IP Address.
+     *
+     * @param string $address
+     * @return string
+     */
+    private function fromLongToBinary(string $address): string
+    {
+        return $this->fromDecimalToBinary($this->fromLongToDecimal($address));
+    }
+
+    /**
+     * Converts a long integer IP Address to dotted binary IP Address.
+     * @param string $address
+     * @return string
+     */
+    private function fromLongToDottedBinary(string $address): string
+    {
+        return $this->fromDecimalToDottedBinary($this->fromLongToDecimal($address));
+    }
+}
