cef49afffaca56d1515e5b72a9f31100bbf0d291
angelcamposm <angel.campos.m@outlook.com>
2021-02-28 21:40:29 +0100

    Added Class HexadecimalIPv4AddressConverter.php

---
+++ src/Converters/HexadecimalIPv4AddressConverter.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Acamposm\IPv4AddressConverter\Converters;
+
+use Acamposm\IPv4AddressConverter\{
+    Enums\IPAddressFormatEnum as IPAddressFormat,
+    Traits\MutableIPv4AddressTrait as MutableIPv4Address,
+};
+
+class HexadecimalIPv4AddressConverter extends BaseAddressConverter
+{
+    use MutableIPv4Address;
+
+    /**
+     * Performs the conversion from the input format to Hexadecimal string.
+     *
+     * @return int|string
+     */
+    public function convert(): int|string
+    {
+        return match ($this->inputFormat) {
+            IPAddressFormat::BINARY => $this->withDotNotation
+                ? $this->fromBinaryToDottedHex($this->address)
+                : $this->fromBinaryToHex($this->address),
+            IPAddressFormat::DECIMAL => $this->withDotNotation
+                ? $this->fromDecimalToDottedHex($this->address)
+                : $this->fromDecimalToHex($this->address),
+            IPAddressFormat::HEXADECIMAL => $this->withDotNotation
+                ? $this->applyDotNotationToAddress($this->address, 2)
+                : $this->address,
+            IPAddressFormat::LONG => $this->withDotNotation ?
+                $this->fromLongToDottedHex($this->address) :
+                $this->fromLongToHex($this->address),
+            default => '',
+        };
+    }
+}
